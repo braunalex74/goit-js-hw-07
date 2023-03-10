b89034fa1bdcbd0079b5cc773498e1130c217423
braunalex74 <braunalex74@googlemail.com>
2023-03-10 21:56:57 +0200

    DZ_7_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -6,7 +6,11 @@ imageContainer.insertAdjacentElement("beforeend", imagesMarkup);
 
 function createImagesModalMarkup(galleryItems) {
   return galleryItems.map(({ preview, original }) => {
-    return ``;
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" data-source="large-image.jpg" alt="Image description"/>
+    </a>
+  </div>`;
   });
 }
 
