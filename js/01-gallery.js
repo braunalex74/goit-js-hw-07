9b9f28aa4ccd84b4274f21926c286d1fa3ae06f0
braunalex74 <braunalex74@googlemail.com>
2023-03-11 15:52:39 +0200

    DZ_7_1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,14 +3,16 @@ import { galleryItems } from "./gallery-items.js";
 const imageContainer = document.querySelector(".gallery");
 const imagesMarkup = createImagesModalMarkup(galleryItems);
 imageContainer.insertAdjacentElement("beforeend", imagesMarkup);
+imageContainer.addEventListener("click", onImageClick);
 
 function createImagesModalMarkup(galleryItems) {
-  return galleryItems.map(({ preview, original }) => {
-    return `<div class="gallery__item">
-    <a class="gallery__link" href="large-image.jpg">
-      <img class="gallery__image" src="small-image.jpg" data-source="large-image.jpg" alt="Image description"/>
-    </a>
-  </div>`;
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+      </a>
+    </div>`;
   });
 }
 
